0fb9443f8667c3a10640754cebc2354772db002c
Le-Yo <lenykoskey@yahoo.com>
2015-08-26 19:39:15 +0300

    updated multi_levels process

--- examples/multi_levels.php
+++ examples/multi_levels.php
@@ -6,26 +6,39 @@ $input = getInput($text);
 
 if($input['level'] == 0){
   //Breaking news, Football, Athletics, Gossip
-
-  $reply = "Safaricom:".PHP_EOL."1.Safaricom+".PHP_EOL."2.M-PESA";
-
+  $reply = self::getMainMenu();
   echo "CON ".$reply;
   exit;
-}else{
+}elseif($input['level'] == 1){
   //get the input form the previous step
   if($input['latest_message'] == 1){
-
-    //safaricom menu
-  $reply = "Still in progress";
-
+      $reply = getSafaricomMenu();
   }elseif($input['latest_message'] == 2){
     //MPESA menu
-    $reply = "MPESA:".PHP_EOL."1. Send Money".PHP_EOL."2.Withdraw Cash".PHP_EOL."3. Buy Airtime";
 
+    $reply = getMpesaMenu();
+    print_r($reply);
+    exit;
   }else{
-    $reply = "We do not understand your response";
+    $reply = getErrorMessage();
     echo "END ".$reply;
-exit;
+   exit;
+  }
+
+echo "CON ".$reply;
+
+}elseif($input['level'] == 2){
+  //get the input form the previous step
+  if($input['exploded_text'][0] == 1){
+    //Safaricom Menu level 2
+  }elseif($input['exploded_text'][0] == 2){
+    //MPESA level 2
+
+    $reply = getMpesaLevel2($input['latest_message']);
+  }else{
+    $reply = getErrorMessage();
+    echo "END ".$reply;
+   exit;
   }
 
 echo "CON ".$reply;
@@ -40,8 +53,38 @@ echo "CON ".$reply;
 
 //end at this
 
+function getMpesaLevel2($latest_message){
 
 
+  if($latest_message == 1){
+      $reply = "Send money process";
+  }elseif($latest_message == 2){
+    //MPESA menu
+    $reply = "Withdraw cash process";
+  }else{
+    $reply = "Buy Airtime Process";
+    echo "END ".$reply;
+   exit;
+  }
+
+  return $reply;
+}
+function getErrorMessage(){
+  return "We do not understand your response";
+
+}
+function getMpesaMenu(){
+  return "MPESA:".PHP_EOL."1.Send Money".PHP_EOL."2.Withdraw Cash".PHP_EOL."3.Buy Airtime";
+
+}
+function getMainMenu(){
+  return "Safaricom:".PHP_EOL."1.Safaricom+".PHP_EOL."2.M-PESA";
+}
+
+function getSafaricomMenu(){
+  return "Still in progress";
+
+}
 function getInput($text){
   //check if text is empty
   $input = array();
@@ -55,7 +98,6 @@ function getInput($text){
       $input['level'] = count($exploded_text);
       $input['latest_message'] = end($exploded_text);
   }
-
 return $input;
 }
 
